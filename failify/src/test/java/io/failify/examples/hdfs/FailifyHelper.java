cb74715594387ad1fe87039d761fd1269581312f
Armin Balalaie <armin.balalaie@gmail.com>
2019-10-15 16:56:17 -0700

    initial test

--- failify/src/test/java/io/failify/examples/hdfs/FailifyHelper.java
+++ failify/src/test/java/io/failify/examples/hdfs/FailifyHelper.java
@@ -6,45 +6,198 @@ import io.failify.dsl.entities.PathAttr;
 import io.failify.dsl.entities.PortType;
 import io.failify.dsl.entities.ServiceType;
 import io.failify.exceptions.RuntimeEngineException;
+import io.failify.execution.CommandResults;
 import io.failify.execution.ULimit;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.hdfs.DFSClient;
+import org.apache.hadoop.hdfs.DFSUtilClient;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
+import org.apache.hadoop.hdfs.protocol.HdfsConstants;
+import org.apache.hadoop.hdfs.server.datanode.DataNodeTestUtils;
+import org.apache.hadoop.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.StringJoiner;
 
 public class FailifyHelper {
     public static final Logger logger = LoggerFactory.getLogger(FailifyHelper.class);
 
-    public static Deployment getDeployment(int numOfDNs) {
+    private static final String CLUSTER_NAME = "mycluster";
+    private static final int NN_HTTP_PORT = 9870;
+    private static final int NN_RPC_PORT = 8020;
+
+    private int numOfDNs;
+    private int numOfNNs;
+    private FailifyRunner runner;
+    private Deployment deployment;
+
+    public FailifyHelper(int numOfNNs, int numOfDNs) {
+        this.numOfDNs = numOfDNs;
+        this.numOfNNs = numOfNNs;
+    }
+
+    public FailifyRunner start() throws RuntimeEngineException {
         String version = "3.1.2"; // this can be dynamically generated from maven metadata
         String dir = "hadoop-" + version;
-        return Deployment.builder("example-hdfs")
-            .withService("zk").dockerImg("failify/zk:3.4.14").dockerFile("docker/zk", true).disableClockDrift().and()
+        deployment = Deployment.builder("example-hdfs")
+            .withService("zk").dockerImg("failify/zk:3.4.14").dockerFile("docker/zk", true)
+                .disableClockDrift().and().withNode("zk1", "zk").and()
             .withService("hadoop-base")
                 .appPath("../hadoop-3.1.2-build/hadoop-dist/target/" + dir + ".tar.gz", "/hadoop", PathAttr.COMPRESSED)
                 .appPath("etc", "/hadoop/" + dir + "/etc").workDir("/hadoop/" + dir)
+                .appPath("etc/hadoop/hdfs-site.xml", "/hadoop/" + dir + "/etc/hadoop/hdfs-site.xml",
+                        new HashMap<String, String>() {{
+                            put("NN_STRING", getNNString());
+                            put("NN_ADDRESSES", getNNAddresses());
+                        }})
                 .env("HADOOP_HOME", "/hadoop/" + dir).env("HADOOP_HEAPSIZE_MAX", "1g")
                 .dockerImg("failify/hadoop:1.0").dockerFile("docker/Dockerfile", true)
                 .logDir("/hadoop/" + dir + "/logs").serviceType(ServiceType.JAVA).and()
-            .withService("nn", "hadoop-base").initCmd("bin/hdfs namenode -bootstrapStandby")
-                .startCmd("bin/hdfs --daemon start zkfc && bin/hdfs --daemon start namenode").tcpPort(50070)
-                .stopCmd("bin/hdfs --daemon stop namenode").and().nodeInstances(3, "nn", "nn", true)
+            .withService("nn", "hadoop-base")
+                .initCmd("bin/hdfs namenode -bootstrapStandby")
+                .startCmd("bin/hdfs --daemon start zkfc && bin/hdfs --daemon start namenode").tcpPort(8020, 9870)
+                .stopCmd("bin/hdfs --daemon stop namenode && bin/hdfs --daemon stop zkfc").and()
+                .nodeInstances(numOfNNs, "nn", "nn", true)
             .withService("dn", "hadoop-base")
                 .startCmd("bin/hdfs --daemon start datanode").stopCmd("bin/hdfs --daemon stop datanode")
                 .and().nodeInstances(numOfDNs, "dn", "dn", true)
             .withService("jn", "hadoop-base")
-                .startCmd("bin/hdfs --daemon start journalnode").stopCmd("bin/hdfs --dae stop journalnode").and()
-            .nodeInstances(3, "jn", "jn", false).withNode("zk1", "zk").and()
+                .startCmd("bin/hdfs --daemon start journalnode").stopCmd("bin/hdfs --daemon stop journalnode").and()
+            .nodeInstances(3, "jn", "jn", false)
             .node("nn1").initCmd("bin/hdfs namenode -format && bin/hdfs zkfc -formatZK").and().build();
+        runner = deployment.start();
+        startNodesInOrder();
+        return runner;
     }
 
-    public static void startNodesInOrder(FailifyRunner runner) throws InterruptedException, RuntimeEngineException {
-        Thread.sleep(10000);
-        runner.runtime().startNode("nn1");
-        Thread.sleep(10000);
-        runner.runtime().startNode("nn2"); runner.runtime().startNode("nn3");
-        for (String node: runner.runtime().nodeNames()) if (node.startsWith("dn")) runner.runtime().startNode(node);
-        Thread.sleep(10000);
+    public void stop() {
+        if (runner != null) {
+            runner.stop();
+        }
     }
+
+    public void startNodesInOrder() throws RuntimeEngineException {
+        try {
+            Thread.sleep(10000);
+            runner.runtime().startNode("nn1");
+            Thread.sleep(15000);
+            runner.runtime().startNode("nn2");
+            runner.runtime().startNode("nn3");
+            for (String node : runner.runtime().nodeNames())
+                if (node.startsWith("dn")) runner.runtime().startNode(node);
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            logger.warn("startNodesInOrder sleep got interrupted");
+        }
+    }
+
+    private String getNNString() {
+        StringJoiner stringJoiner = new StringJoiner(",");
+        for (int i=1; i<=numOfNNs; i++) {
+            stringJoiner.add("nn" + i);
+        }
+        return stringJoiner.toString();
+    }
+
+    private String getNNAddresses() {
+        String addrTemplate =
+                "    <property>\n" +
+                        "        <name>dfs.namenode.rpc-address.mycluster.{{NAME}}</name>\n" +
+                        "        <value>{{NAME}}:8020</value>\n" +
+                        "    </property>\n" +
+                        "    <property>\n" +
+                        "        <name>dfs.namenode.http-address.mycluster.{{NAME}}</name>\n" +
+                        "        <value>{{NAME}}:9870</value>\n" +
+                        "    </property>";
+
+        String retStr = "";
+        for (int i=1; i<=numOfNNs; i++) {
+            retStr += addrTemplate.replace("{{NAME}}", "nn" + i);
+        }
+        return retStr;
+    }
+
+    public FailifyRunner runner() {
+        return runner;
+    }
+
+    public Deployment deployment() {
+        return deployment;
+    }
+
+    public FileSystem getFileSystem() throws IOException {
+        return FileSystem.get(getConfiguration());
+    }
+
+    public Configuration getConfiguration() {
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://" + CLUSTER_NAME);
+        conf.set("dfs.client.failover.proxy.provider."+ CLUSTER_NAME,
+                "org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider");
+        conf.set("dfs.nameservices", CLUSTER_NAME);
+        conf.set("dfs.ha.namenodes."+ CLUSTER_NAME, getNNString());
+
+        for (int i=1; i<=numOfNNs; i++) {
+            String nnIp = runner.runtime().ip("nn" + i);
+            conf.set("dfs.namenode.rpc-address."+ CLUSTER_NAME +".nn" + i, nnIp + ":" +
+                    runner.runtime().portMapping("nn" + i, NN_RPC_PORT, PortType.TCP));
+            conf.set("dfs.namenode.http-address."+ CLUSTER_NAME +".nn" + i, nnIp + ":" +
+                    runner.runtime().portMapping("nn" + i, NN_HTTP_PORT, PortType.TCP));
+        }
+
+        return conf;
+    }
+
+    private InetSocketAddress getNNRpcAddress(int index) {
+        return new InetSocketAddress(runner.runtime().ip("nn" + index),
+                runner.runtime().portMapping("nn" + index, NN_RPC_PORT, PortType.TCP));
+    }
+
+    private InetSocketAddress getNNHttpAddress(int index) {
+        return new InetSocketAddress(runner.runtime().ip("nn" + index),
+                runner.runtime().portMapping("nn" + index, NN_HTTP_PORT, PortType.TCP));
+    }
+
+    public void waitActive() throws RuntimeEngineException {
+
+        for (int index=1; index<= numOfNNs; index++) {
+            for (int retry=3; retry>0; retry--){
+                logger.info("Checking if NN nn{} is UP (retries left {})", index, retry-1);
+                if (this.assertNNisUpAndReceivingReport(index, numOfDNs))
+                    continue;
+
+                if (retry > 1) {
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        logger.warn("waitActive sleep got interrupted");
+                    }
+                }
+            }
+            throw new RuntimeException("NN nn" + index + " is not active or not receiving reports from DNs");
+        }
+        logger.info("The cluster is ACTIVE");
+    }
+
+    public boolean assertNNisUpAndReceivingReport(int index, int numOfDNs) throws RuntimeEngineException {
+        if (runner.runtime().runCommandInNode("nn" + index, "bin/hdfs haadmin -ns " + CLUSTER_NAME + " -checkHealth nn" + index)
+                .exitCode() != 0)
+            return false;
+
+        CommandResults res = runner.runtime().runCommandInNode("nn" + index, "bin/hdfs dfsadmin -report");
+        if (res.exitCode() == 0 && res.stdOut().contains("Live datanodes (" + numOfDNs + ")"))
+            return true;
+
+        return false;
+    }
+
+
+
+
 }
--- failify/src/test/java/io/failify/examples/hdfs/SampleTest.java
+++ failify/src/test/java/io/failify/examples/hdfs/SampleTest.java
@@ -2,40 +2,150 @@ package io.failify.examples.hdfs;
 
 import io.failify.FailifyRunner;
 import io.failify.exceptions.RuntimeEngineException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.AppendTestUtil;
+import org.apache.hadoop.hdfs.DFSConfigKeys;
+import org.apache.hadoop.hdfs.DFSTestUtil;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hdfs.protocol.ExtendedBlock;
+import org.apache.hadoop.hdfs.protocolPB.DatanodeProtocolClientSideTranslatorPB;
+import org.apache.hadoop.hdfs.server.blockmanagement.DatanodeDescriptor;
+import org.apache.hadoop.hdfs.server.datanode.DataNode;
+import org.apache.hadoop.hdfs.server.namenode.NameNode;
+import org.apache.hadoop.hdfs.server.namenode.ha.HATestUtil;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.test.GenericTestUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 public class SampleTest {
     private static final Logger logger = LoggerFactory.getLogger(SampleTest.class);
 
-    protected static FailifyRunner runner;
     protected static final int NUM_OF_DNS = 3;
 
-    @BeforeClass
-    public static void before() throws RuntimeEngineException, InterruptedException {
-        runner = FailifyHelper.getDeployment(NUM_OF_DNS).start();
-        FailifyHelper.startNodesInOrder(runner);
+    private static final int BLOCK_SIZE = 4096;
+    private static final int BLOCK_AND_A_HALF = BLOCK_SIZE * 3 / 2;
+
+    private static final Path TEST_PATH =
+            new Path("/test-file");
+
+    protected static final Logger LOG = LoggerFactory.getLogger(
+            SampleTest.class);
 
 
-        logger.info("The cluster is UP!");
-    }
-
-    @AfterClass
-    public static void after() {
-        if (runner != null) {
-            runner.stop();
-        }
-    }
-
-
+    /**
+     * Test the scenario where the NN fails over after issuing a block
+     * synchronization request, but before it is committed. The
+     * DN running the recovery should then fail to commit the synchronization
+     * and a later retry will succeed.
+     */
     @Test
-    public void sampleTest() throws RuntimeEngineException, SQLException, ClassNotFoundException, TimeoutException {
+    public void testFailoverRightBeforeCommitSynchronization() throws IOException, RuntimeEngineException {
+        final Configuration conf = new Configuration();
+        // Disable permissions so that another user can recover the lease.
+        conf.setBoolean(DFSConfigKeys.DFS_PERMISSIONS_ENABLED_KEY, false);
+        conf.setInt(DFSConfigKeys.DFS_BLOCK_SIZE_KEY, BLOCK_SIZE);
 
+        FSDataOutputStream stm = null;
+
+        FailifyHelper failifyHelper = new FailifyHelper(3, NUM_OF_DNS);
+        FailifyRunner runner = failifyHelper.start();
+        failifyHelper.waitActive();
+        logger.info("The cluster is UP!");
+
+        FileSystem fs = failifyHelper.getFileSystem();
+
+        fs.create(new Path("/test.txt"));
+
+        logger.info(" {} ", fs.listFiles(new Path("/"), false).next());
+        runner.stop();
+
+//        final MiniDFSCluster cluster = newMiniCluster(conf, 3);
+//        try {
+//            cluster.waitActive();
+//            cluster.transitionToActive(0);
+//            Thread.sleep(500);
+//
+//            LOG.info("Starting with NN 0 active");
+//            FileSystem fs = HATestUtil.configureFailoverFs(cluster, conf);
+//            stm = fs.create(TEST_PATH);
+//
+//            // write a half block
+//            AppendTestUtil.write(stm, 0, BLOCK_SIZE / 2);
+//            stm.hflush();
+//
+//            // Look into the block manager on the active node for the block
+//            // under construction.
+//
+//            NameNode nn0 = cluster.getNameNode(0);
+//            ExtendedBlock blk = DFSTestUtil.getFirstBlock(fs, TEST_PATH);
+//            DatanodeDescriptor expectedPrimary =
+//                    DFSTestUtil.getExpectedPrimaryNode(nn0, blk);
+//            LOG.info("Expecting block recovery to be triggered on DN " +
+//                    expectedPrimary);
+//
+//            // Find the corresponding DN daemon, and spy on its connection to the
+//            // active.
+//            DataNode primaryDN = cluster.getDataNode(expectedPrimary.getIpcPort());
+//            DatanodeProtocolClientSideTranslatorPB nnSpy =
+//                    InternalDataNodeTestUtils.spyOnBposToNN(primaryDN, nn0);
+//
+//            // Delay the commitBlockSynchronization call
+//            DelayAnswer delayer = new DelayAnswer(LOG);
+//            Mockito.doAnswer(delayer).when(nnSpy).commitBlockSynchronization(
+//                    Mockito.eq(blk),
+//                    Mockito.anyLong(), // new genstamp
+//                    Mockito.anyLong(), // new length
+//                    Mockito.eq(true), // close file
+//                    Mockito.eq(false), // delete block
+//                    Mockito.any(),  // new targets
+//                    Mockito.any()); // new target storages
+//
+//            DistributedFileSystem fsOtherUser = createFsAsOtherUser(cluster, conf);
+//            assertFalse(fsOtherUser.recoverLease(TEST_PATH));
+//
+//            LOG.info("Waiting for commitBlockSynchronization call from primary");
+//            delayer.waitForCall();
+//
+//            LOG.info("Failing over to NN 1");
+//
+//            cluster.transitionToStandby(0);
+//            cluster.transitionToActive(1);
+//
+//            // Let the commitBlockSynchronization call go through, and check that
+//            // it failed with the correct exception.
+//            delayer.proceed();
+//            delayer.waitForResult();
+//            Throwable t = delayer.getThrown();
+//            if (t == null) {
+//                fail("commitBlockSynchronization call did not fail on standby");
+//            }
+//            GenericTestUtils.assertExceptionContains(
+//                    "Operation category WRITE is not supported",
+//                    t);
+//
+//            // Now, if we try again to recover the block, it should succeed on the new
+//            // active.
+//            loopRecoverLease(fsOtherUser, TEST_PATH);
+//
+//            AppendTestUtil.check(fs, TEST_PATH, BLOCK_SIZE/2);
+//        } finally {
+//            IOUtils.closeStream(stm);
+//            cluster.shutdown();
+//            runner.stop();
+//        }
     }
 }
